f4b59a079af13b9bd387e78ee38671f76d3d72e1
Monika Choudhary <Monika.Choudhary@philips.com>
2025-11-08 14:51:19 +0530

    Correction in Statistics.cs and StatsUnitTest.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Stats
+    {
+        public double average { get; set; }
+        public double min { get; set; }
+        public double max { get; set; }
+    }
+
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers)
         {
-            //Implement statistics here
+            var stats = new Stats();
+            if (numbers == null || numbers.Count == 0)
+            {
+                stats.average = Double.NaN;
+                stats.min = Double.NaN;
+                stats.max = Double.NaN;
+                return stats;
+            }
+            double sum = 0;
+            double min = Double.MaxValue;
+            double max = Double.MinValue;
+            foreach (var num in numbers)
+            {
+                sum += num;
+                if (num < min) min = num;
+                if (num > max) max = num;
+            }
+            stats.average = sum / numbers.Count;
+            stats.min = min;
+            stats.max = max;
+            return stats;
         }
     }
 }
